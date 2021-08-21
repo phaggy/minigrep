566de99e51335789d423a25cc9e6656e34ca30dc
phaggy <rynmalikp@gmail.com>
2021-08-21 11:40:18 +0530

    first add

---
+++ src/lib.rs
@@ -0,0 +1,39 @@
+use std::{error::Error, fs};
+
+#[derive(Debug)]
+pub struct Config {
+    pub filename: String,
+    pub pattern: String,
+}
+
+impl Config {
+    pub fn new(args: &[String]) -> Result<Config, &str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+        return Ok(Config {
+            filename: args[2].clone(),
+            pattern: args[1].clone(),
+        });
+    }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let file_contents = fs::read_to_string(config.filename)?;
+    for line in search(&config.pattern, &file_contents) {
+        println!("{}", line);
+    }
+    Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
---
+++ src/main.rs
@@ -0,0 +1,18 @@
+use std::{env, process};
+
+use minigrep::{run, Config};
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("\nproblem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    if let Err(err) = run(config) {
+        // config gets transferred to run
+        eprintln!("\ncouldnt readfile: {}", err);
+        process::exit(1);
+    };
+}
